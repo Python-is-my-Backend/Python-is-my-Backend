7b6327304da89a7594acf196365c49e2a3a619bd
rpixpunk <79791897+rpixpunk@users.noreply.github.com>
2024-04-21 10:38:28 -0400

    Update exercise.py

--- App/controllers/exercise.py
+++ App/controllers/exercise.py
@@ -3,21 +3,42 @@ from App.models import Exercise
 from App.database import db
 
 def parse_exercises():
-  api_url = 'https://api.api-ninjas.com/v1/exercises?muscle'
-  exercises = requests.get(api_url, headers={'X-Api-Key': '34ECD4LQjf7k/KSQABanUg==kRrodx9CuPCV9pAH'}).json()
-  
-  for exercise in exercises:
-    new_exercise = Exercise(
-        name=exercise['name'],
-        type=exercise['type'],
-        muscle=exercise['muscle'],
-        equipment=exercise['equipment'],
-        difficulty=exercise['difficulty'],
-        instructions=exercise['instructions']
-    )
+    abdominals_exercises = find_exercises('abdominals')
+    abductors_exercises = find_exercises('abductors')
+    adductors_exercises = find_exercises('adductors')
+    biceps_exercises = find_exercises('biceps')
+    calves_exercises = find_exercises('calves')
+    chest_exercises = find_exercises('chest')
+    forearms_exercises = find_exercises('forearms')
+    glutes_exercises = find_exercises('glutes')
+    hamstrings_exercises = find_exercises('hamstrings')
+    lats_exercises = find_exercises('lats')
+    lower_back_exercises = find_exercises('lower_back')
+    middle_back_exercises = find_exercises('middle_back')
+    neck_exercises = find_exercises('neck')
+    quadriceps_exercises = find_exercises('quadriceps')
+    traps_exercises = find_exercises('traps')
+    triceps_exercises = find_exercises('triceps')
 
-    db.session.add(new_exercise)
-  db.session.commit()
+def find_exercises(muscle_group):
+    api_url = 'https://api.api-ninjas.com/v1/exercises?muscle={}'.format(muscle_group)
+    exercises = requests.get(api_url, headers={'X-Api-Key': '34ECD4LQjf7k/KSQABanUg==kRrodx9CuPCV9pAH'}).json()
+
+    for exercise in exercises:
+        new_exercise = Exercise(
+            name=exercise['name'],
+            type=exercise['type'],
+            muscle=exercise['muscle'],
+            equipment=exercise['equipment'],
+            difficulty=exercise['difficulty'],
+            instructions=exercise['instructions']
+        )
+
+        db.session.add(new_exercise)
+    db.session.commit()
+
+def get_all_exercises_by_muscle(muscle):
+    return Exercise.query.filter_by(muscle=muscle)
 
 def get_all_exercises():
   return Exercise.query.all()
