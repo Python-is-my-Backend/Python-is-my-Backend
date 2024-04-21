4501be7b9241aa376e8289035ca0b85217cff2b9
rpixpunk <79791897+rpixpunk@users.noreply.github.com>
2024-04-21 19:15:02 -0400

    Update user.py

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -30,4 +30,34 @@ def update_user(id, username):
         db.session.add(user)
         return db.session.commit()
     return None
-    
+
+def create_routine(self, exercise_id, name):
+    exercise = Routine.query.get(exercise_id)
+    if exercise:
+        try:
+            new_exercise = Routine(name, self.id, exercise_id)
+            db.session.add(new_exercise)
+            db.session.commit()
+            return new_exercise
+        except Exception as e:
+            print(e)
+            db.session.rollback()
+            return None
+    return None
+
+def remove_routine(self, exercise_id):
+    exercise = Routine.query.get(exercise_id)
+    if exercise.user == self:
+        db.session.delete(exercise)
+        db.session.commit()
+        return True
+    return None
+
+def rename_routine(self, exercise_id, name):
+    exercise = Routine.query.get(exercise_id)
+    if exercise.user == self:
+        exercise.name = name
+        db.session.add(exercise)
+        db.session.commit()
+        return True
+    return None
