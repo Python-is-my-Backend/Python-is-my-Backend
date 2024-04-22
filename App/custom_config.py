ad925fe89887b75d7c537615fcece3929412d337
AnuRam34 <100068619+AnuRam34@users.noreply.github.com>
2024-04-21 23:32:03 -0400

    Add files via upload

---
+++ App/custom_config.py
@@ -0,0 +1,4 @@
+JWT_ACCESS_TOKEN_EXPIRES = 7
+SQLALCHEMY_DATABASE_URI = "postgresql://uwi_courses_database_user:BotgGRv4tFjnyudGhwZs4jWZu7pFivUs@dpg-cog0vpv79t8c73cdbaeg-a/uwi_courses_database"
+SECRET_KEY = "secret key"
+ENV = "PRODUCTION"
